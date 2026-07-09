5234293d5fda27eef34365f86af64802692aa2c4
emrocode <92260849+emrocode@users.noreply.github.com>
2026-07-09 16:52:07 -0300

    chore: update app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
-import { Archivo } from "next/font/google";
 import "./globals.css";
 
-const fontSans = Archivo({
-  variable: "--font-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Archivo } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const archivo = Archivo({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Style",
@@ -15,12 +14,6 @@ export const metadata: Metadata = {
     index: false,
     follow: false,
   },
-  icons: {
-    icon: [
-      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-    ],
-  },
 };
 
 export default function RootLayout({
@@ -29,8 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fontSans.variable} antialiased`}>{children}</body>
+    <html
+      lang="en"
+      className={cn("antialiased", "font-sans", archivo.variable)}
+    >
+      <body>{children}</body>
     </html>
   );
 }
